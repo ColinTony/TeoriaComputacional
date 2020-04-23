a27e49558b6eb63f3dcee6a3bdca215841439d3c
ColinTony <colincitoheredia@gmail.com>
2020-04-22 19:22:01 -0500

    Empezando con archivos

---
+++ Codigo/Practica4(Chess)/Practica4(Chess)/src/practica4/chess/Controllers/ArchivosGenerator.java
@@ -0,0 +1,10 @@
+package practica4.chess.Controllers;
+
+/**
+ *
+ * @author colin
+ * Clase que se dedicara a escribir los archivos de caminos posibles
+ */
+public class ArchivosGenerator {
+    
+}
